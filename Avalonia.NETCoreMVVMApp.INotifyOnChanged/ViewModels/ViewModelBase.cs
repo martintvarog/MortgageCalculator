b125fdbaa2be9af2d5a1d4aca9b8f076c8eae5f0
Tvarog <m_tvarog@utb.cz>
2022-10-02 20:41:09 +0200

    init commit

---
+++ Avalonia.NETCoreMVVMApp.INotifyOnChanged/ViewModels/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace Avalonia.NETCoreMVVMApp.INotifyOnChanged.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Avalonia.NETCoreMVVMApp.INotifyOnChanged/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Avalonia.NETCoreMVVMApp.INotifyOnChanged.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
