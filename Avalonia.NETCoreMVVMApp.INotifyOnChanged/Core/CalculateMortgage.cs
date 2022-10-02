4519ff063d9482d9438767b095124a671a3e9a64
Tvarog <m_tvarog@utb.cz>
2022-10-02 21:19:49 +0200

    Payment Logic

---
+++ Avalonia.NETCoreMVVMApp.INotifyOnChanged/Core/CalculateMortgage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Avalonia.NETCoreMVVMApp.INotifyOnChanged.Core;
+
+internal static class CalculateMortgage
+{
+    //calculate overall mortage payment
+    internal static double CalculateOverallMortagePayment(int loanAmount, double interestRate, int termInYears)
+    {
+        double monthlyInterestRate = interestRate / 12;
+        double numberOfPayments = termInYears * 12;
+        double payment = (loanAmount * monthlyInterestRate) / (1 - Math.Pow(1 + monthlyInterestRate, -numberOfPayments));
+        double overallPayment = payment * numberOfPayments;
+        return overallPayment;
+    }
+    
+    //calculate monthly payment
+    internal static double CalculateMonthlyPayment(int loanAmount, double interestRate, int termInYears)
+    {
+        double monthlyInterestRate = interestRate / 12;
+        double numberOfPayments = termInYears * 12;
+        double payment = (loanAmount * monthlyInterestRate) / (1 - Math.Pow(1 + monthlyInterestRate, -numberOfPayments));
+        return payment;
+    }
+}
