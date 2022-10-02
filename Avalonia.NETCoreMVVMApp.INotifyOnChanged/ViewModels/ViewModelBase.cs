8d7b23c1f43f09cb19dea19bd726515dd24102a1
Tvarog <m_tvarog@utb.cz>
2022-10-02 21:20:30 +0200

    props added

--- Avalonia.NETCoreMVVMApp.INotifyOnChanged/ViewModels/ViewModelBase.cs
+++ Avalonia.NETCoreMVVMApp.INotifyOnChanged/ViewModels/ViewModelBase.cs
@@ -6,21 +6,48 @@ namespace Avalonia.NETCoreMVVMApp.INotifyOnChanged.ViewModels
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        private string _name;
-
-        public string Name
-        {
-            get => _name;
-            set
-            {
-                _name = value;
-                OnPropertyChanged(nameof(Name));
-            }
-        }
+        public double MonthlyPayment { get; set; }
+        public double TotalPayment { get; set; }
 
         private void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private int _loanAmount;
+
+        public int LoanAmount
+        {
+            get => _loanAmount;
+            set
+            {
+                _loanAmount = value;
+                OnPropertyChanged(nameof(LoanAmount));
+            }
+        }
+
+        private double _interestRate;
+
+        public double InterestRate
+        {
+            get => _interestRate;
+            set
+            {
+                _interestRate = value;
+                OnPropertyChanged(nameof(InterestRate));
+            }
+        }
+
+        private int _termInYears;
+
+        public int TermInYears
+        {
+            get => _termInYears;
+            set
+            {
+                _termInYears = value;
+                OnPropertyChanged(nameof(TermInYears));
+            }
         }
     }
 }
